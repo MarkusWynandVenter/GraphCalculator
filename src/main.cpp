7c37fd1a4c431217e109fbf6584db065d553c44b
Markus Wynand Venter <markuswynandventer@gmail.com>
2023-04-29 23:13:00 +0200

    Added Files:     src/.vs/ProjectSettings.json     src/.vs/VSWorkspaceState.json     src/.vs/slnx.sqlite     src/.vs/src/FileContentIndex/16a997fb-a3f0-48ab-87ad-cf4b58c44678.vsidx     src/.vs/src/FileContentIndex/6e3d8798-ab66-4db0-bd86-229af986c93c.vsidx     src/.vs/src/FileContentIndex/read.lock     src/.vs/src/v17/.wsuo     src/.vs/src/v17/Browse.VC.db     src/Makefile     src/main.cpp
    Modified Files:
        src/.vs/src/v17/.suo
        src/.vs/src/v17/Browse.VC.db
        src/main.cpp
    
    Added the make file to see if it would compile

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include "equationContext.h"
 #include "equationSolver.h"
 #include "equationDifferentiate.h"
+#include "equationFindX.h"
+#include "equationSimplify.h"
+
 
 using namespace std;
 
@@ -11,6 +14,12 @@ int main (int argc, char *argv[]) {
     EquationContext equationContext;
 
     equationContext.setSolver(make_unique<EquationDifferentiate>());
+    equationContext.solveEquation();
+
+    equationContext.setSolver(make_unique<EquationFindX>());
+    equationContext.solveEquation();
+
+    equationContext.setSolver(make_unique<EquationSimplify>());
     equationContext.solveEquation();
 
     return 0;
