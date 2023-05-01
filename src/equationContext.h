71073e7c6e56d0eb4471bc9e05e898c3d863bb4c
MarkusWynandVenter <markuswynandventer@gmail.com>
2023-05-01 20:31:26 +0200

    Files modified: src/Makefile src/equationContext.h src/equationDifferentiate.cpp src/equationDifferentiate.h src/equationFindX.cpp src/equationFindX.h src/equationSimplify.cpp src/equationSimplify.h src/equationSolver.cpp src/equationSolver.h src/main.cpp src/toString.cpp src/toString.h
    Program now compiles and runs.  It does not do anything useful yet.

--- src/equationContext.h
+++ src/equationContext.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <memory>
 #include "equationSolver.h"
 #include <iostream>
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <memory>
 #include "equationContext.h"
 #include "equationSolver.h"
 #include "equationDifferentiate.h"
